5a4f768f0537368ae3a6cc2bca80f4ecaf1201f0
Zagrosi <sz10g21@soton.ac.uk>
2024-04-23 20:20:49 +0100

    Add new GQL queries for PR1, PR2, and PR3 in ScriptAutoTestPR1.ps1

--- ScriptAutoTestPR1.ps1
+++ ScriptAutoTestPR1.ps1
@@ -1,5 +1,19 @@
+Write-Host "Start Testing Process" -ForegroundColor Blue
 .\ScriptClean.ps1
 .\ScriptBuild.ps1
+
+Write-Host "Start Testing PR1" -ForegroundColor Blue
 .\GQL .\queries\pr1.gql
+Write-Host "End Testing PR1" -ForegroundColor Blue
+
+Write-Host "Start Testing PR2" -ForegroundColor Blue
 .\GQL .\queries\pr2.gql
+Write-Host "End Testing PR2" -ForegroundColor Blue
+
+Write-Host "Start Testing PR3" -ForegroundColor Blue
+.\GQL .\queries\pr3.gql
+Write-Host "End Testing PR3" -ForegroundColor Blue
+
 .\ScriptClean.ps1
+
+Write-Host "End Testing Process" -ForegroundColor Blue
