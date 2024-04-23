61b756e7f400f97ed926b113f187017ee1af4391
Zagrosi <sz10g21@soton.ac.uk>
2024-04-23 21:22:03 +0100

    Update PowerShell script names for consistency

--- ScriptPSAutoTest.ps1
+++ ScriptPSAutoTest.ps1
@@ -1,6 +1,6 @@
 Write-Host "Start Testing Process" -ForegroundColor Blue
-.\ScriptClean.ps1
-.\ScriptBuild.ps1
+.\ScriptPSClean.ps1
+.\ScriptPSBuild.ps1
 
 Write-Host "Start Testing PR1" -ForegroundColor Blue
 .\GQL .\queries\pr1.gql
@@ -14,6 +14,6 @@ Write-Host "Start Testing PR3" -ForegroundColor Blue
 .\GQL .\queries\pr3.gql
 Write-Host "End Testing PR3" -ForegroundColor Blue
 
-.\ScriptClean.ps1
+.\ScriptPSClean.ps1
 
 Write-Host "End Testing Process" -ForegroundColor Blue
