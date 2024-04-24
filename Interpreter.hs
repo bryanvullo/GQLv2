072bf6401fd8ac5d31b9171b077d88b0c230c47d
bryanvullo <bryanvullo04@gmail.com>
2024-04-24 03:01:44 +0100

    pseudocode for the rest of the commands

--- Interpreter.hs
+++ Interpreter.hs
@@ -15,3 +15,14 @@ interpret (s : statements) varibales =
             interpret statements newGraph
 
         -- interpret all the other statements
+        {-
+        FIND -> reduce the amount of rows in the graph to only the ones that match the query
+            use a loop to go through each row and check if it matches the query
+        OUT -> print the graph using Printer.hs
+            Output must be a graph, ie Table or [Row] 
+        CONDITIONS -> interpret the boolean expression, if true then interpret the statements
+        LOOPF -> fetch the data we are looping and create a function here to loop over that data, 
+            interpreting each row (node)
+        Bool expressions -> trivial - use haskell's built in functions
+        REMOVE -> remove the data from the graph, ie row from the table
+        -}
