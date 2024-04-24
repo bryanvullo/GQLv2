70430395d19d421c841f53dc5a36618517b68be0
Zagrosi <sz10g21@soton.ac.uk>
2024-04-24 20:30:02 +0100

    Fix undefined function and update functions in Interpreter.hs

--- Interpreter.hs
+++ Interpreter.hs
@@ -57,20 +57,75 @@ interpretClass cls rows = undefined
 findTable :: String -> Tables -> [Row]
 findTable label tables = undefined
 
+-- findTable :: String -> Tables -> [Row]
+-- findTable label tables = case lookup label (zip (map getTableLabel tables) tables) of
+--     Just rows -> rows
+--     Nothing -> []
+--   where
+--     getTableLabel :: Table -> String
+--     getTableLabel ((Header _):_) = "Header"
+--     getTableLabel ((LabeledHeader _):_) = "LabeledHeader"
+--     getTableLabel ((RelationshipHeader _):_) = "RelationshipHeader"
+--     getTableLabel ((Data (Id label) _):_) = label
+--     getTableLabel ((LabeledData (Id label) _ _):_) = label
+--     getTableLabel ((RelationshipData (Id label) _ _ _):_) = label
+
 updateTable :: [Row] -> [Row] -> [Row]
 updateTable rows1 rows2 = undefined
+
+-- updateTable :: [Row] -> [Row] -> [Row]
+-- updateTable [] rows2 = rows2
+-- updateTable rows1 [] = rows1
+-- updateTable (row1:rows1) (row2:rows2) = updatedRow : updateTable rows1 rows2
+--   where
+--     updatedRow = case (row1, row2) of
+--         (Data id1 values1, Data _ values2) -> Data id1 (updateValues values1 values2)
+--         (LabeledData id1 values1 labels1, LabeledData _ values2 labels2) ->
+--             LabeledData id1 (updateValues values1 values2) (labels1 ++ labels2)
+--         (RelationshipData id1 values1 id2 rel, RelationshipData _ values2 _ _) ->
+--             RelationshipData id1 (updateValues values1 values2) id2 rel
+--         _ -> row1
 
 updateValues :: [Value] -> [Value] -> [Value]
 updateValues values1 values2 = undefined
 
+-- updateValues :: [Value] -> [Value] -> [Value]
+-- updateValues [] values2 = values2
+-- updateValues values1 [] = values1
+-- updateValues (val1:vals1) (val2:vals2) = updatedValue : updateValues vals1 vals2
+--   where
+--     updatedValue = case (val1, val2) of
+--         (NullValue, _) -> val2
+--         (_, NullValue) -> val1
+--         _ -> val2
+
 getAttribute :: String -> [Row] -> Value
 getAttribute attr rows = undefined
+
+-- getAttribute :: String -> [Row] -> Value
+-- getAttribute _ [] = NullValue
+-- getAttribute attr (row:rows) = case row of
+--     Data _ values -> getValue attr values
+--     LabeledData _ values _ -> getValue attr values
+--     RelationshipData _ values _ _ -> getValue attr values
+--     _ -> getAttribute attr rows
 
 getValue :: String -> [Value] -> Value
 getValue attr values = undefined
 
+-- getValue :: String -> [Value] -> Value
+-- getValue _ [] = NullValue
+-- getValue attr (value:values) = case value of
+--     StringValue str | attr == str -> value
+--     IntValue i | attr == show i -> value
+--     BoolValue b | attr == show b -> value
+--     _ -> getValue attr values
+
 filterTable :: (Row -> Bool) -> [Row] -> [Row]
 filterTable predicate rows = undefined
+
+-- filterTable :: (Row -> Bool) -> [Row] -> [Row]
+-- filterTable predicate = filter predicate
 
 isMatchingRow :: Row -> String -> Bool
 isMatchingRow row regex = undefined
@@ -99,20 +154,50 @@ idToString (Id str) = str
 numericIncrease :: [Row] -> Int -> [Row]
 numericIncrease rows value = undefined
 
+-- numericIncrease :: [Row] -> Int -> [Row]
+-- numericIncrease = map . incrementNumericValues
+
 numericDecrease :: [Row] -> Int -> [Row]
 numericDecrease rows value = undefined
+
+-- numericDecrease :: [Row] -> Int -> [Row]
+-- numericDecrease = map . decrementNumericValues
 
 incrementNumericValues :: Int -> Row -> Row
 incrementNumericValues value row = undefined
 
+-- incrementNumericValues :: Int -> Row -> Row
+-- incrementNumericValues value row = case row of
+--     Data id values -> Data id (map (incrementValue value) values)
+--     LabeledData id values labels -> LabeledData id (map (incrementValue value) values) labels
+--     RelationshipData id1 values id2 rel -> RelationshipData id1 (map (incrementValue value) values) id2 rel
+--     _ -> row
+
 decrementNumericValues :: Int -> Row -> Row
 decrementNumericValues value row = undefined
+
+-- decrementNumericValues :: Int -> Row -> Row
+-- decrementNumericValues value row = case row of
+--     Data id values -> Data id (map (decrementValue value) values)
+--     LabeledData id values labels -> LabeledData id (map (decrementValue value) values) labels
+--     RelationshipData id1 values id2 rel -> RelationshipData id1 (map (decrementValue value) values) id2 rel
+--     _ -> row
 
 incrementValue :: Int -> Value -> Value
 incrementValue value val = undefined
 
+-- incrementValue :: Int -> Value -> Value
+-- incrementValue value val = case val of
+--     IntValue i -> IntValue (i + value)
+--     _ -> val
+
 decrementValue :: Int -> Value -> Value
 decrementValue value val = undefined
+
+-- decrementValue :: Int -> Value -> Value
+-- decrementValue value val = case val of
+--     IntValue i -> IntValue (i - value)
+--     _ -> val
 
 getNumericValue :: [Row] -> Int
 getNumericValue rows = undefined
