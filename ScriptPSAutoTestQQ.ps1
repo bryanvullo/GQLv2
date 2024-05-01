5dcd54eded7c30f0ecdf51f284edf258138672c2
Zagrosi <sz10g21@soton.ac.uk>
2024-05-01 18:06:35 +0100

    Refactor GQL queries in pr1.gql, pr2.gql, pr3.gql, pr5.gql, pr6.gql, pr8.gql, pr10.gql for improved readability and maintainability

--- ScriptPSAutoTestQQ.ps1
+++ ScriptPSAutoTestQQ.ps1
@@ -5,43 +5,43 @@ Write-Host "Start Testing Process" -ForegroundColor Blue
 .\ScriptPSBuild.ps1
 
 Write-Host "Start Testing PR1" -ForegroundColor Blue
-.\GQL .\queries\pr1.gql
+.\GQL pr1.gql
 Write-Host "End Testing PR1" -ForegroundColor Blue
 
 Write-Host "Start Testing PR2" -ForegroundColor Blue
-.\GQL .\queries\pr2.gql
+.\GQL pr2.gql
 Write-Host "End Testing PR2" -ForegroundColor Blue
 
 Write-Host "Start Testing PR3" -ForegroundColor Blue
-.\GQL .\queries\pr3.gql
+.\GQL pr3.gql
 Write-Host "End Testing PR3" -ForegroundColor Blue
 
 Write-Host "Start Testing PR4" -ForegroundColor Blue
-.\GQL .\queries\pr4.gql
+.\GQL pr4.gql
 Write-Host "End Testing PR4" -ForegroundColor Blue
 
 Write-Host "Start Testing PR5" -ForegroundColor Blue
-.\GQL .\queries\pr5.gql
+.\GQL pr5.gql
 Write-Host "End Testing PR5" -ForegroundColor Blue
 
 Write-Host "Start Testing PR6" -ForegroundColor Blue
-.\GQL .\queries\pr6.gql
+.\GQL pr6.gql
 Write-Host "End Testing PR6" -ForegroundColor Blue
 
 Write-Host "Start Testing PR7" -ForegroundColor Blue
-.\GQL .\queries\pr7.gql
+.\GQL pr7.gql
 Write-Host "End Testing PR7" -ForegroundColor Blue
 
 Write-Host "Start Testing PR8" -ForegroundColor Blue
-.\GQL .\queries\pr8.gql
+.\GQL pr8.gql
 Write-Host "End Testing PR8" -ForegroundColor Blue
 
 Write-Host "Start Testing PR9" -ForegroundColor Blue
-.\GQL .\queries\pr9.gql
+.\GQL pr9.gql
 Write-Host "End Testing PR9" -ForegroundColor Blue
 
 Write-Host "Start Testing PR10" -ForegroundColor Blue
-.\GQL .\queries\pr10.gql
+.\GQL pr10.gql
 Write-Host "End Testing PR10" -ForegroundColor Blue
 
 Write-Host "Start Cleaning Process" -ForegroundColor Blue
