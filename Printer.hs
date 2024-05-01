e73c57e182b5b1b306244158d67d24dd732d9663
Zagrosi <sz10g21@soton.ac.uk>
2024-05-01 22:45:23 +0100

    Refactor Printer module to improve table printing functionality

--- Printer.hs
+++ Printer.hs
@@ -124,13 +124,16 @@ data GraphValue = S String | Ss [String] | I Int | B Bool | Null
 
 printOutput :: [[(String, String, GraphValue)]] -> IO ()
 printOutput nodes = do
-    let tables = groupNodesToTables nodes
+    let attributeTypes = getAttributeTypes nodes
+    let nodeRows = map nodeToRow nodes
+    let tables = groupNodesToTables attributeTypes nodeRows
     mapM_ printTable (Map.elems tables)
 
-printTable :: (String, [(String, String)]) -> IO ()
+printTable :: ([(String, String)], [[(String, String)]]) -> IO ()
 printTable (header, rows) = do
-    putStrLn header
-    mapM_ (putStrLn . snd) rows
+    let headerStr = intercalate "," $ map (\(k, t) -> k ++ ":" ++ t) header
+    putStrLn headerStr
+    mapM_ (putStrLn . intercalate ",") rows
     putStrLn ""
 
 printRow :: Row -> IO ()
@@ -202,19 +205,22 @@ labelsToString :: Labels -> String
 labelsToString (l:labels) = ", " ++ l ++ labelsToString labels
 labelsToString [] = ""
 
-printTables :: [[(String, String, GraphValue)]] -> IO ()
-printTables nodes = do
-    let tables = groupNodesToTables nodes
-    mapM_ printTable (Map.elems tables)
+getAttributeTypes :: [[(String, String, GraphValue)]] -> [(String, String)]
+getAttributeTypes nodes = Map.toList $ Map.fromListWith (\t1 t2 -> t1) [(k, t) | node <- nodes, (k, t, _) <- node]
 
-groupNodesToTables :: [[(String, String, GraphValue)]] -> Map String (String, [(String, String)])
-groupNodesToTables nodes = Map.fromListWith (++) [(getHeader node, [(getHeader node, nodeToRow node)]) | node <- nodes]
+nodeToRow :: [(String, String, GraphValue)] -> [(String, String)]
+nodeToRow node = [(k, valueToString v) | (k, _, v) <- node]
     where
-        getHeader = intercalate "," . map (\(k, t, _) -> k ++ ":" ++ t) . takeWhile (\(k, _, _) -> k /= "LABEL")
-        nodeToRow = intercalate "," . map (\(k, _, v) -> valueToString v)
         valueToString (I x) = show x
         valueToString (S x) = x
         valueToString (Ss xs) = intercalate ";" xs
         valueToString (B True) = "true"
         valueToString (B False) = "false"
         valueToString Null = "null"
+
+groupNodesToTables :: [(String, String)] -> [[(String, String)]] -> Map String ([(String, String)], [[(String, String)]])
+groupNodesToTables attributeTypes nodeRows = Map.fromListWith mergeRows [(getHeader row, ([getAttribute k t | (k, t) <- attributeTypes], [row])) | row <- nodeRows]
+    where
+        getHeader = intercalate "," . map fst
+        getAttribute k t = (k, t)
+        mergeRows (h1, r1) (_, r2) = (h1, r1 ++ r2)
